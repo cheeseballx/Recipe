818aa1132ccfa6c9d6cb3aa42984b4b34e07e228
Andreas Gladisch (P52) <andreas.gladisch@outlook.com>
2020-01-16 20:55:09 +0100

    finish the RecipeClass and the Tag class

--- src/main/java/de/andy/web/recipe/model/Recipe.java
+++ src/main/java/de/andy/web/recipe/model/Recipe.java
@@ -6,9 +6,22 @@ import de.andy.web.recipe.model.Ingrident;
 
 public class Recipe{
 
-    private ArrayList<Ingrident> ingridients;
+
+    private Long id;
+    private ArrayList<Ingrident> ingridents;
     private ArrayList<TextPart> textParts;
     private ArrayList<Tag> tags;
 
 
+    public Long getId(){ return id;}
+    public ArrayList<Ingrident> getIngridents(){ return ingridents; }
+    public ArrayList<TextPart> getTextParts(){ return textParts; }
+    public ArrayList<Tag> getTags() { return tags;}
+
+    public void setId( Long id){ this.id = id; }
+    public void setIngridents( ArrayList<Ingrident> ingridents) { this.ingridents = ingridents; }
+    public void setTextParts( ArrayList<TextPart> textParts) { this.textParts = textParts; }
+    public void setTags(ArrayList<Tag> tags) { this.tags = tags; }
+
+    
 }
--- src/main/java/de/andy/web/recipe/model/Tag.java
+++ src/main/java/de/andy/web/recipe/model/Tag.java
@@ -1,5 +1,18 @@
 package de.andy.web.recipe.model;
 
 public class Tag{
+    Long id;
+    String name;
+
+    public Tag(String name){
+        this.name = name;
+        this.id = -1L;
+    }
+
+    public void setName(String name){ this.name = name; }
+    public void setId(Long id){ this.id = id; }
+
+    public Long getId(){ return this.id; }
+    public String getName() { return this.name; }
 
 }
