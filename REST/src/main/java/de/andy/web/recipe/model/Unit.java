dc3f1b233882e288e798bf9ae5e55016b81cafe1
Andreas Gladisch (P52) <andreas.gladisch@outlook.com>
2020-03-12 20:33:20 +0100

    del bins

--- REST/src/main/java/de/andy/web/recipe/model/Unit.java
+++ REST/src/main/java/de/andy/web/recipe/model/Unit.java
@@ -1,9 +1,24 @@
 package de.andy.web.recipe.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Column;
+import javax.persistence.Id;
 
+@Entity
 public class Unit {
 
-    public Unit(){ 
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private int test;
+
+    protected Unit(){}
+
+    public Unit(int test){
     }
+
 }
