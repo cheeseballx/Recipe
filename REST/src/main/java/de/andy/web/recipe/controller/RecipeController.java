1eaefa8be9e4a9b8a033e6951d0aebd044aa7bc4
Andreas Gladisch (P52) <andreas.gladisch@outlook.com>
2021-11-13 00:03:23 +0100

    form basic react UI Shape

--- REST/src/main/java/de/andy/web/recipe/controller/RecipeController.java
+++ REST/src/main/java/de/andy/web/recipe/controller/RecipeController.java
@@ -15,6 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,6 +24,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/v1/Recipe")
 @Tag(name = "Recipes", description = "Controlling the Recipes")
 public class RecipeController{
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+
+const RecipeID = "1010";
+const URL = "http://127.0.0.1:3002/v1/Recipe/" + RecipeID;
 
 function App() {
+
+  const [data,setData] = useState({})
+
+  useEffect(() => {
+    fetch(URL)
+    .then(response => response.json())
+    .then(result => setData(result));
+  },[]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{data.name}</h1>
+      <h2>{data.longname}</h2>
+      <h4>{data.description}</h4>
+
+      {data.components && data.components.map(c=> {
+        return <p key={c.id}>{c.amount} {c.ingrident.unit} {c.ingrident.name}</p>
+      })}
     </div>
   );
 }
