34fb289cc1d5ed9b2d6e812742fe6f6708272002
Andreas Gladisch (P52) <andreas.gladisch@outlook.com>
2022-08-21 17:25:41 +0200

    show pictures with slideshow

--- frontend/src/Reciepe.jsx
+++ frontend/src/Reciepe.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 import { Link } from 'react-router-dom';
+import SimpleImageSlider from "react-simple-image-slider";
 
 const URL = "http://127.0.0.1:3002/v1/Recipe/";
 
@@ -19,7 +20,14 @@ function Recipe() {
     <div>
       <h1>{data.name}</h1>
       <h2>{data.longname}</h2>
-      {data.images && data.images.map(pic => <img src={"img"+pic.url} /> )}
+
+      {data.images && <SimpleImageSlider
+        width={896} height={504}
+        showBullets={true} showNavs={true}
+        images={data.images.map(x=> "img"+x.url)}
+        onClick = {(idx, event) =>{}}
+      />}
+
       <h4>{data.description}</h4>
 
       {data.components && data.components.map(c=> {
